4ec4ebf57621473bfa2d07618b7444d163833029
JudithT <administrator@Christophers-MacBook-Pro.local>
2019-07-08 19:55:25 -0700

    refactor model.py 3

--- server.py
+++ server.py
@@ -12,7 +12,6 @@ app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'postgresql:///recipes'
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Required to use Flask sessions and the debug toolbar
 
 try:
@@ -271,6 +270,7 @@ if __name__ == "__main__":
     app.jinja_env.auto_reload = app.debug
     db.app = app
     db.init_app(app)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.create_all()
 
     # Use the DebugToolbar
